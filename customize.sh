3da99a60e62cebb95c00a9b614464a41975008bd
7008500 <7008500@qq.com>
2021-07-06 19:09:34 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -13,7 +13,7 @@ sed -i 's/m25p,fast-read/broken-flash-reset/g' openwrt/target/linux/ramips/dts/m
 sed -i 's/0xf60000/0x1f60000/g' openwrt/target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/15744k/32448k/g' openwrt/target/linux/ramips/image/mt7621.mk
 sed -i 's/kmod-mt7615d luci-app-mtwifi -wpad-openssl/kmod-mt7615d_dbdc/g' openwrt/target/linux/ramips/image/mt7621.mk
-git clone https://github.com/LuttyYang/luci-theme-material openwrt/package/luci-theme-material
+git clone https://github.com/weweyes/luci-app-argon-config openwrt/package/luci-app-argon-config
 git clone https://github.com/tty228/luci-app-serverchan openwrt/package/luci-app-serverchan
 git clone https://github.com/destan19/OpenAppFilter openwrt/package/OpenAppFilter
 git clone https://github.com/garypang13/luci-app-eqos openwrt/package/luci-app-eqos
