bc15e2682cad6270cc73012aabb3cdd849b77c4c
7008500 <7008500@qq.com>
2021-07-18 22:51:38 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -12,11 +12,9 @@ sed -i 's/80000000/10000000/g' openwrt/target/linux/ramips/dts/mt7621_phicomm_k2
 sed -i 's/m25p,fast-read/broken-flash-reset/g' openwrt/target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/0xf60000/0x1f60000/g' openwrt/target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/15744k/32448k/g' openwrt/target/linux/ramips/image/mt7621.mk
-sed -i 's/kmod-mt7615d luci-app-mtwifi -wpad-openssl/kmod-mt7615d_dbdc/g' openwrt/target/linux/ramips/image/mt7621.mk
 git clone https://github.com/weweyes/luci-app-argon-config openwrt/package/luci-app-argon-config
 git clone https://github.com/tty228/luci-app-serverchan openwrt/package/luci-app-serverchan
 git clone https://github.com/destan19/OpenAppFilter openwrt/package/OpenAppFilter
 git clone https://github.com/garypang13/luci-app-eqos openwrt/package/luci-app-eqos
 rm -rf openwrt/package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon openwrt/package/lean/luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' openwrt/feeds/luci/collections/luci/Makefile
