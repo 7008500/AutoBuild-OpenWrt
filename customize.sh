70d495f996155eef6c5650d452558b0bf034da56
7008500 <7008500@qq.com>
2021-10-14 00:22:34 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -14,6 +14,7 @@ sed -i 's/0xf60000/0x1f60000/g' openwrt/target/linux/ramips/dts/mt7621_phicomm_k
 sed -i 's/15744k/32448k/g' openwrt/target/linux/ramips/image/mt7621.mk
 git clone https://github.com/weweyes/luci-app-argon-config openwrt/package/luci-app-argon-config
 git clone https://github.com/tty228/luci-app-serverchan openwrt/package/luci-app-serverchan
+sed -i 's/sctapi.ftqq.com/sc.ftqq.com/g' openwrt/package/luci-app-serverchan/root/usr/bin/serverchan/api/serverchan.json
 git clone https://github.com/destan19/OpenAppFilter openwrt/package/OpenAppFilter
 git clone https://github.com/garypang13/luci-app-eqos openwrt/package/luci-app-eqos
 rm -rf openwrt/package/lean/luci-theme-argon
