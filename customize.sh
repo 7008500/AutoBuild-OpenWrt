7d8d653cc99dc385c4221605a1520153264fcae0
7008500 <7008500@qq.com>
2022-07-15 19:05:18 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -12,10 +12,10 @@ sed -i 's/80000000/10000000/g' openwrt/target/linux/ramips/dts/mt7621_phicomm_k2
 sed -i 's/m25p,fast-read/broken-flash-reset/g' openwrt/target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/0xf60000/0x1f60000/g' openwrt/target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/15744k/32448k/g' openwrt/target/linux/ramips/image/mt7621.mk
-git clone https://github.com/weweyes/luci-app-argon-config openwrt/package/luci-app-argon-config
-git clone https://github.com/tty228/luci-app-serverchan openwrt/package/luci-app-serverchan
+git clone https://github.com/7008500/luci-app-argon-config openwrt/package/luci-app-argon-config
+git clone https://github.com/7008500/luci-app-serverchan openwrt/package/luci-app-serverchan
 sed -i 's/sctapi.ftqq.com/sc.ftqq.com/g' openwrt/package/luci-app-serverchan/root/usr/bin/serverchan/api/serverchan.json
-git clone https://github.com/destan19/OpenAppFilter openwrt/package/OpenAppFilter
-git clone https://github.com/garypang13/luci-app-eqos openwrt/package/luci-app-eqos
+git clone https://github.com/7008500/OpenAppFilter openwrt/package/OpenAppFilter
+git clone https://github.com/7008500/luci-app-unblockneteasemusic openwrt/package/luci-app-unblockneteasemusic
 rm -rf openwrt/package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon openwrt/package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/7008500/luci-theme-argon openwrt/package/lean/luci-theme-argon
