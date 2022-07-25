6f993bb1f42c1a9a4623eccf25b4559151ac98dc
7008500 <7008500@qq.com>
2022-07-25 22:08:34 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -13,9 +13,6 @@ sed -i 's/m25p,fast-read/broken-flash-reset/g' openwrt/target/linux/ramips/dts/m
 sed -i 's/0xf60000/0x1f60000/g' openwrt/target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/15744k/32448k/g' openwrt/target/linux/ramips/image/mt7621.mk
 git clone https://github.com/7008500/luci-app-argon-config openwrt/package/luci-app-argon-config
-git clone https://github.com/tty228/luci-app-serverchan openwrt/package/luci-app-serverchan
-sed -i 's/sctapi.ftqq.com/sc.ftqq.com/g' openwrt/package/luci-app-serverchan/root/usr/bin/serverchan/api/serverchan.json
-git clone https://github.com/7008500/OpenAppFilter openwrt/package/OpenAppFilter
-git clone https://github.com/7008500/luci-app-unblockneteasemusic openwrt/package/luci-app-unblockneteasemusic
+sed -i 's/sctapi.ftqq.com/sc.ftqq.com/g' openwrt/feeds/luci/applications/luci-app-serverchan/root/usr/bin/serverchan/api/serverchan.json
 rm -rf openwrt/feeds/luci/themes/luci-theme-argon
 git clone -b 18.06 https://github.com/7008500/luci-theme-argon openwrt/package/lean/luci-theme-argon
